ce085284d4a049d66f514d80911e745f2c018d6b
duongvancuong <vancuong279dn@gmail.com>
2019-05-07 17:25:15 +0700

    authentication with graphql

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_090443) do
+ActiveRecord::Schema.define(version: 2019_05_07_100934) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
